a45455bbd3a1de8ad479610c8055da0c16b9b55a
markhwang <seungjune.hwang@gmail.com>
2022-08-09 15:58:32 +0900

    MVC 시작

---
+++ src/main/java/com/google/mvc/ControllerServlet.java
@@ -0,0 +1,51 @@
+package com.google.mvc;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ControllerServlet", value = "*.gg")
+public class ControllerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("헬로 월드");
+        String uri = request.getRequestURI();
+        String url = request.getRequestURL().toString();
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("getRequestURI: " + uri);
+        out.println("<br>");
+        out.println("getRequestURL: " + url);
+        // 만약에 main.gg 가 들어오면 메인 페이지 화면
+        if (uri.equals("/main.gg")) {
+            //out.println("<br>메인페이지");
+            RequestDispatcher dis = request.getRequestDispatcher("main.jsp");
+            dis.forward(request, response);
+        } else if (uri.equals("/movie.gg")) {
+            out.println("<br>영화 페이지");
+        } else if (uri.equals("/search.gg")) {
+            out.println("<br>검색 페이지");
+        } else if (uri.equals("/foodrandom.gg")) {
+            out.println("<br>음식을 랜덤으로 골라주는 페이지");
+        } else if (uri.equals("/login.gg")) {
+            out.println("<br>로그인 페이지");
+        } else if (uri.equals("/naver.gg")) {
+            response.sendRedirect("https://www.naver.com");
+        } else {
+            out.println("잘못 접근 페이지");
+        }
+        // movie.gg 가 들어오면 영화정보
+        // search.gg 가 들어오면 검색화면
+        // foodrandom.gg 가 들어오면 음식을 랜덤으로 골라주는 페이지
+        // login.gg 가 들어오면 로그인창
+        // 회원가입 등등...
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
---
+++ src/main/webapp/main.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>메인페이지</title>
+</head>
+<body>
+<h1><%= "메인 페이지!" %>
+</h1>
+<br/>
+<a href="/main.gg">메인</a><br>
+<a href="/movie.gg">영화</a><br>
+<a href="/search.gg">검색</a><br>
+<a href="/foodrandom.gg">음식추천</a><br>
+</body>
+</html>
